0633ebef1a67faa1c9398b5c4bbc5fa807f32e70
srideep-banerjee <banerjee.srideep@gmail.com>
2024-04-05 21:45:23 +0530

    Improved notes item list display

---
+++ lib/models/notes_item.dart
@@ -0,0 +1,5 @@
+class NotesItem {
+  final String title;
+  final String content;
+  const NotesItem({required this.title, required this.content});
+}
--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:notes_flutter/ui/home/profile_icon.dart';
+import 'package:notes_flutter/models/notes_item.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  late Future<List<NotesItem>> notesListFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    print("initState Started");
+    notesListFuture = createList(10);
+    print("initState complete");
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print("Home page build called");
     return Scaffold(
       appBar: AppBar(
         title: const Text("Notes"),
@@ -17,19 +35,47 @@ class HomePage extends StatelessWidget {
         onPressed: () {},
         child: const Icon(Icons.add),
       ),
-      body: ListView.builder(
-        itemCount: 50000,
-        itemBuilder: (context, index) {
-          return NotesItemDisplay(
-            "Item: $index",
-            onClick: () {
-              
-            },
-          );
+      body: FutureBuilder(
+        future: notesListFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            print("Displaying List");
+
+            if (snapshot.data!.isEmpty) {
+              return const Center(
+                child: Text("No notes created")
+              );
+            }
+
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return NotesItemDisplay(
+                  snapshot.data![index].title,
+                  onClick: () {
+                    print("Item at index: $index clicked");
+                  },
+                );
+              },
+            );
+          } else {
+            print("Notes List is loading");
+            return const Center(
+              child: Text("Loading ...")
+            );
+          }
         },
       ),
     );
   }
+
+  Future<List<NotesItem>> createList(int length) async {
+    return List<NotesItem>.generate(
+        length, (index) => NotesItem(title: "title $index", content: "content $index")
+    );
+  }
+
+
 }
 
 class NotesItemDisplay extends StatelessWidget {
