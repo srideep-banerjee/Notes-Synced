17dcf049b3d19dea8d8edf38f5a7f093e934a223
srideep-banerjee <banerjee.srideep@gmail.com>
2024-05-07 22:01:48 +0530

    Added multiple helper functions in FirestoreHelper

--- lib/firebase/firestore_helper.dart
+++ lib/firebase/firestore_helper.dart
@@ -1,6 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:notes_flutter/default_settings.dart';
 import 'package:notes_flutter/firebase/firebase_emulator.dart';
+import 'package:notes_flutter/firebase/firestore_note_model.dart';
+import 'package:notes_flutter/firebase/notes_change_model.dart';
+import 'package:notes_flutter/firebase/user_document_model.dart';
+import 'package:notes_flutter/models/notes_item.dart';
 
 class FirestoreHelper {
 
@@ -19,16 +24,131 @@ class FirestoreHelper {
       return firestore;
     });
 
-  Stream<String?> getLastUpdatedStream(String uid) async* {
+  Future<void> createUserDocument(String uid) async {
     FirebaseFirestore firestore = await _futureFirebaseFirestore;
-    yield* firestore
-        .collection("users").doc(uid).snapshots()
-        .map(
-            (DocumentSnapshot<Map<String, dynamic>> snapshot) {
-              return snapshot.data()?["last_updated"];
-            }
+
+    await firestore.doc("users/$uid").set(
+      {"last_updated": FieldValue.serverTimestamp()},
+      SetOptions(merge: true),
     );
   }
 
+  Future<UserDocumentModel?> getUserDocument(String uid) async {
+    FirebaseFirestore firestore = await _futureFirebaseFirestore;
 
+    UserDocumentModel? from(
+        DocumentSnapshot<Map<String, dynamic>> snapshot,
+        SnapshotOptions? options
+        ) {
+      if (!snapshot.exists) return null;
+      return UserDocumentModel.fromMap(snapshot.data()!);
+    }
+
+    Map<String, Object?> to(UserDocumentModel? userDocumentModel, SetOptions? options) {
+      return userDocumentModel?.toMap() ?? <String, Object?>{};
+    }
+
+    return (
+        await firestore
+            .doc("users/$uid")
+            .withConverter(fromFirestore: from, toFirestore: to)
+            .get()
+    ).data();
+  }
+
+  Future<List<String>> getNewDocumentIds(String uid,int count) async {
+    FirebaseFirestore firestore = await _futureFirebaseFirestore;
+    CollectionReference colRef = firestore.collection("users/$uid/notes");
+    List<String> newIds = [];
+    for (int i = 0; i < count; i++) {
+      newIds.add(colRef.doc().id);
+    }
+    return newIds;
+  }
+
+  Future<void> setNote(String uid, String firestoreId, NotesItem newData) async {
+    FirebaseFirestore firestore = await _futureFirebaseFirestore;
+    WriteBatch batch = firestore.batch();
+    DocumentReference docRef = firestore.doc("users/$uid/notes/$firestoreId");
+    Map<String, dynamic> data = newData.toMap();
+    data["last_updated"] = FieldValue.serverTimestamp();
+    batch.set(docRef, data);
+    batch.set(
+      firestore.doc("users/$uid"),
+      {"last_updated": FieldValue.serverTimestamp()},
+      SetOptions(merge: true),
+    );
+    await batch.commit();
+  }
+
+  Future<void> upsertAllNotes(String uid, List<FirestoreNoteModel> notes) async {
+    FirebaseFirestore firestore = await _futureFirebaseFirestore;
+    WriteBatch batch = firestore.batch();
+
+    batch.set(
+      firestore.doc("users/$uid"),
+      {"last_updated": FieldValue.serverTimestamp()},
+      SetOptions(merge: true),
+    );
+    for (FirestoreNoteModel note in notes) {
+      var data = note.toMap();
+      data["last_updated"] = FieldValue.serverTimestamp();
+      batch.set(
+        firestore.doc("users/$uid/notes/${note.documentId}"),
+        data,
+        SetOptions(merge: true),
+      );
+    }
+    await batch.commit();
+  }
+
+  Future<List<FirestoreNoteModel>> getUpdatedNoteList(String lastUpdated, String uid) async {
+    FirebaseFirestore firestore = await _futureFirebaseFirestore;
+    QuerySnapshot<Map<String, dynamic>> querySnapshot = await firestore
+        .collection("users/$uid/notes/")
+        .where(DefaultSettings.lastUpdatedKeyName, isGreaterThan: lastUpdated)
+        .get();
+
+    return querySnapshot.docs.map((value) {
+      return FirestoreNoteModel.fromMap(value.data(), value.id);
+    }).toList();
+  }
+
+  /// Gets a streams of notes changes after lastUpdated for the user uid
+  Stream<List<NotesChangeModel>> getNoteQueryStream(String lastUpdated, String uid) async* {
+    FirebaseFirestore firestore = await _futureFirebaseFirestore;
+
+    FirestoreNoteModel? from(DocumentSnapshot<Map<String, dynamic>> snapshot, SnapshotOptions? options) {
+
+      if (!snapshot.exists) return null;
+      return FirestoreNoteModel.fromMap(snapshot.data()!, snapshot.id);
+    }
+
+    Map<String, Object?> to(FirestoreNoteModel? model, SetOptions? options) {
+
+      return model?.toMap() ?? <String, Object?>{};
+    }
+
+    yield* firestore.collection("users/$uid/notes")
+        .withConverter(fromFirestore: from, toFirestore: to)
+        .where(DefaultSettings.lastUpdatedKeyName, isGreaterThan: lastUpdated)
+        .snapshots()
+        .map(
+            (querySnapshot) => querySnapshot
+                .docChanges
+                .map (_docChangeToNotesChangeModel)
+                .toList()
+    );
+  }
+
+  NotesChangeModel _docChangeToNotesChangeModel(DocumentChange<FirestoreNoteModel?> docChange) {
+    DocumentSnapshot<FirestoreNoteModel?> doc = docChange.doc;
+    var newDoc = docChange.doc.data();
+    var deletedId = docChange.type == DocumentChangeType.removed? doc.id : null;
+    return NotesChangeModel(
+      docChange.type,
+      newData: newDoc,
+      deletedId: deletedId,
+    );
+  }
 }
